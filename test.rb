737276730cf9abff90aeba7a64de6e7a3e8276b8
Netto Farah <nettofarah@gmail.com>
2012-03-31 15:31:22 -0300

    removing old test code

--- test.rb
+++
@@ -1,9 +0,0 @@
-require 'bundler'
-Bundler.require
-
-Dir.glob(File.dirname(__FILE__) + '/config/*') {|file| require file}
-Dir.glob(File.dirname(__FILE__) + '/lib/*') {|file| require file}
-
-Geocoder::Configuration.lookup = :yahoo
-
-puts Venue.first.address.geocoded?
