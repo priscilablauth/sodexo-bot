a78fcebd5131697280b3fbfcfc9fcdf0e848f803
Netto Farah <nettofarah@gmail.com>
2012-03-31 15:07:59 -0300

    task to scrap data based on given city and state

--- bot.rb
+++ bot.rb
@@ -1,34 +1,26 @@
 class SodexoBot
 
-  def run(selected_state, selected_city)
-    spider = Spider::Sodexo.new
-    parser = Spider::VenueParser.new
+  def initialize
+    @spider = Spider::Sodexo.new
+    @parser = Spider::VenueParser.new
+  end
 
-    db = {}
-    states = spider.states
-    states = [selected_state] #only RS
-
-    states.each do |state|
-      db[state.to_sym] = {}
-      cities = spider.cities(state)
-      cities = [selected_city]
-
-      cities.each do |city|
-        results = spider.search(state, city)
-
-        venues = results.map do |result|
-          begin
-            venue = parser.parse(result)
-            venue.save
-            venue
-          rescue Exception => e
-            puts "#{e} -> #{result}"
-          end
-        end
-
-        db[state.to_sym][city.to_sym] = venues
-        spider.go_to_search_page
+  def run(state, city)
+    results = @spider.search(state, city)
+    venues = results.map do |result|
+      begin
+        store(result)
+      rescue Exception => e
+        puts "#{e} -> #{result}"
       end
     end
+    @spider.go_to_search_page
+  end
+
+  private
+  def store(result)
+    venue = @parser.parse(result)
+    venue.save
+    venue
   end
 end
